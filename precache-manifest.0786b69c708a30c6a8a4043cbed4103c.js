e8132504f01af5712e1eb72711d83a04310cbaff
David Tenorio <dtenorio.work@gmail.com>
2020-08-06 17:01:52 -0600

    Updates

---
+++ precache-manifest.0786b69c708a30c6a8a4043cbed4103c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cdad49cc1c67d4933fb2c853b7923ff",
+    "url": "/sample-GH-page-react/index.html"
+  },
+  {
+    "revision": "08a9facd1ae7c6a3461b",
+    "url": "/sample-GH-page-react/static/css/main.f6c80b33.chunk.css"
+  },
+  {
+    "revision": "fda3dc81372ea57bec87",
+    "url": "/sample-GH-page-react/static/js/2.fb5d1307.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sample-GH-page-react/static/js/2.fb5d1307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a9facd1ae7c6a3461b",
+    "url": "/sample-GH-page-react/static/js/main.1a3bfd08.chunk.js"
+  },
+  {
+    "revision": "7540976ac27819751794",
+    "url": "/sample-GH-page-react/static/js/runtime-main.224603d3.js"
+  }
+]);
